398828af6445b8bb84bf3d9aa1bafff01b64883b
miguelgerbi <miguelaraujogerbi@gmail.com>
2026-01-28 16:11:41 -0300

    Novo

---
+++ script.js
@@ -0,0 +1,7 @@
+async function obterDados(e){
+    fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+    .then(resp => resp.json())
+    .then(resp => {
+        e.innerHTML = resp.USDBRL.bid
+    })
+}
